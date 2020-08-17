48ed5662ce9e0823c7f99b2710e52aa455574fda
Emil Martinov <martinov.emil@gmail.com>
2020-08-18 00:36:48 +0200

    Random interval button clicker

---
+++ background.js
@@ -0,0 +1,41 @@
+
+// setTimeout(() => {
+//     const buttons = document.querySelectorAll('button');
+//     console.log("Running")
+//     const refreshButtons = Array.prototype.filter.call(buttons, button => button.innerText === 'REFRESH');
+//     refreshButtons.forEach(refreshButton => refreshButton.click())
+// }, 3000);
+
+
+const minuteToMilliSeconds = (minute) => Math.floor(minute * 60_000);
+const minuteArrayToMilliSecondsArray = minutesArray => minutesArray.map(minuteToMilliSeconds)
+const generateRandomNumberWithinRange = (min, max) => () => Math.random() * (max - min) + min;
+const generateRandomArray = (length, min, max) => Array.from(new Array(length), generateRandomNumberWithinRange(min, max));
+let randomPick = (array) => {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+// initial values for buttonText and array
+const buttonText = "REFRESH";
+// each refresh of the browser you get different intervals
+const finalArrayInMilliSeconds = minuteArrayToMilliSecondsArray(generateRandomArray(10, 10, 30)) // size of array, min and max minutes
+console.log(finalArrayInMilliSeconds.map(x => x / 60_000))
+// Look for all the button elements and click the ones with buttonText as their innertext
+const findButtonAndClick = (buttonText) => {
+    const buttons = document.querySelectorAll('button');
+    console.log("Running")
+    const refreshButtons = Array.prototype.filter.call(buttons, button => button.innerText === buttonText);
+    refreshButtons.forEach(refreshButton => refreshButton.click())
+    console.log(`${buttonText} buttons are clicked.`)
+}
+
+
+// recursively call setTimeout with different time intervals and click the button
+const loopTimeOut = (buttonText, finalArrayInMilliSeconds) => setTimeout(() => {
+    findButtonAndClick(buttonText);
+    loopTimeOut(buttonText, finalArrayInMilliSeconds)
+}, randomPick(finalArrayInMilliSeconds))
+
+// let's kick start our process
+loopTimeOut(buttonText, finalArrayInMilliSeconds)
+
